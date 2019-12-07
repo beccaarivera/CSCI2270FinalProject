f24a6b4fa8fec8e47828c030fa54e0a276e940cd
beccaarivera <57190088+beccaarivera@users.noreply.github.com>
2019-12-07 15:46:16 -0700

    updating main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include "CuckooHashing.h"
 #include "LinearProbing.h"
+#include "LL.hpp"
 
 using namespace std;
 
@@ -13,7 +14,7 @@ void mainMenu() {
 	cout << "2. Chaining with BST" << endl;
 	cout << "3. Linear Probing" << endl;
 	cout << "4. Cuckoo Hashing" << endl;
-	//cout << "5. Quit" << endl;
+	cout << "5. Quit" << endl;
 }
 
 void hashMenu() {
@@ -24,86 +25,92 @@ void hashMenu() {
 
 int main() {
 	// prompt user to chosse which collision resolution method to use
-	mainMenu();
 	int mainChoice;
 	int hashChoice;
-	cin >> mainChoice;
+
+  hashLL ll;
 
 
 	//int quit = 0;
 	//while(quit == 0){
-	if (mainChoice == 1) {
-		// prompt user to choose hash function to implement
-		hashMenu();
-		cin >> hashChoice;
-
-		switch (hashChoice) {
-			// x mod TABLE_SIZE
-		case 1:
-			cout << "in hash 1" << endl; // delete couts later
-			break;
-
-			// Floor(x/TABLE_SIZE) mod TABLE_SIZE
-		case 2:
-			cout << "in hash 2" << endl;
-			break;
-		}
-
-		// chaining with BST
-	}
-	else if (mainChoice == 2) {
-		// prompt user to choose hash function to implement
-		hashMenu();
-
-		cin >> hashChoice;
-
-		switch (hashChoice) {
-			// x mod TABLE_SIZE
-		case 1:
-			cout << "in hash 1" << endl;
-			break;
-
-			// Floor(x/TABLE_SIZE) mod TABLE_SIZE
-		case 2:
-			cout << "in hash 2" << endl;
-			break;
-		}
-		break;
-
-		// linear probing
-	}
-	else if (mainChoice == 3) {
-		// prompt user to choose hash function to implement
-		hashMenu();
-		while (true) {
+	while (true) {
+		mainMenu();
+		cin >> mainChoice;
+		if (mainChoice == 1) {
+			// prompt user to choose hash function to implement
+			hashMenu();
 			cin >> hashChoice;
-			if (hashChoice == 1) {
-				LinearProbing hashTable = LinearProbing(1);
+
+			switch (hashChoice) {
+				// x mod TABLE_SIZE
+			case 1:
+				if (ll.isEmpty(0))
+          cout << "is empty, worked!" << endl;
+        else
+          cout << "somethin's wrong" << endl;
+
+			break;
+
+				// Floor(x/TABLE_SIZE) mod TABLE_SIZE
+			case 2:
 
 				break;
 			}
-			else if (hashChoice == 2) {
-				LinearProbing hashTable = LinearProbing(2);
 
-				break;
-			}
-			else {
-				cout << "Invalid hash choice." << endl;
-				hashMenu();
-			}
+			// chaining with BST
 		}
-		// cuckoo hashing
-	}
-	else if (mainChoice == 4) {
-		// uses both hash functions by definition
-		CuckooHashing hashTable;
-	}
-	else if (mainChoice == 5) {
-		break;
-	}
-	}else{
-		cout << "Please enter a number 1-5" << endl;
-	}
-	//}
-}
+		else if (mainChoice == 2) {
+			// prompt user to choose hash function to implement
+			hashMenu();
 
+			cin >> hashChoice;
+
+			switch (hashChoice) {
+				// x mod TABLE_SIZE
+			case 1:
+				cout << "in hash 1" << endl;
+				break;
+
+				// Floor(x/TABLE_SIZE) mod TABLE_SIZE
+			case 2:
+				cout << "in hash 2" << endl;
+				break;
+			}
+
+			// linear probing
+		}
+		else if (mainChoice == 3) {
+			// prompt user to choose hash function to implement
+			hashMenu();
+			while (true) {
+				cin >> hashChoice;
+				if (hashChoice == 1) {
+					LinearProbing hashTable = LinearProbing(1);
+
+					break;
+				}
+				else if (hashChoice == 2) {
+					LinearProbing hashTable = LinearProbing(2);
+
+					break;
+				}
+				else {
+					cout << "Invalid hash choice." << endl;
+					hashMenu();
+				}
+			}
+			// cuckoo hashing
+		}
+		else if (mainChoice == 4) {
+			// uses both hash functions by definition
+			CuckooHashing hashTable;
+		}
+		else if (mainChoice == 5) {
+			cout << "Quitting..." << endl;
+			return 0;
+		}
+		else {
+			cout << "Please enter a number 1-5" << endl;
+		}
+	}
+}
